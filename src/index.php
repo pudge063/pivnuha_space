02cacba3a676ba61c23b4f623e0cb2d8b381c3a7
Administrator <studentq.work@example.com>
2024-09-15 22:03:30 +0400

    db insert

--- src/index.php
+++ src/index.php
@@ -26,7 +26,7 @@
     <h1>Example</h1>
     <?php
 
-    $conn = new mysqli("db", "admin", "123", "db_test");
+    $conn = new mysqli("pivnuha.space", "admin", "123", "db_test");
     if ($conn->connect_error) {
         die("Ошибка: " . $conn->connect_error);
     }
@@ -34,11 +34,11 @@
     if ($result = $conn->query($sql)) {
         $rowsCount = $result->num_rows;
         echo "<p>Всего записей: $rowsCount</p>";
-        echo "<table class='index-table'><tr><th>Id</th><th>Имя</th></tr>";
+        echo "<table class='index-table'><tr><th>Имя</th><th>Пиво</th></tr>";
         foreach ($result as $row) {
             echo "<tr>";
-            echo "<td>" . $row["id"] . "</td>";
             echo "<td>" . $row["name"] . "</td>";
+            echo "<td>" . $row["beer"] . "</td>";
             echo "</tr>";
         }
         echo "</table>";
@@ -47,6 +47,38 @@
         echo "Ошибка: " . $conn->error;
     }
     $conn->close();
+
+    ?>
+
+    <h2>Отметиться</h2>
+    <form action="" method="post" class="form-example">
+        <div class="form-example">
+            <label for="name">Имя: </label>
+            <input type="text" name="name" id="name" required />
+        </div>
+        <div class="form-example">
+            <label for="beer">Пиво: </label>
+            <input type="text" name="beer" id="beer" required />
+        </div>
+        <div class="form-example">
+            <input type="submit" value="Отправить!" />
+        </div>
+    </form>
+
+    <?php
+    if (isset($_POST['name']) && isset($_POST['text'])) {
+
+        $name = $_POST['name'];
+        $beer = $_POST['beer'];
+
+        $data = array("name" => $name, "beer" => $beer);
+        $query = $conn->prepare("INSERT INTO test1 (name, beer) values (:name, :beer)");
+        $query->execute($data);
+
+        if ($result) {
+            echo "Информация занесена в базу данных";
+        }
+    }
 
     ?>
 
