b0b421e200ee0d4e9dcca559c955ad996a27d627
Administrator <studentq.work@example.com>
2024-09-16 00:39:10 +0400

    fix

--- src/index.php
+++ src/index.php
@@ -23,8 +23,8 @@
         </header>
 
         <?php
-        define($MYSQL_USER, getenv('MYSQL_USER'));
-        define($MYSQL_PASSWORD, getenv('MYSQL_PASSWORD'));
+        define($MYSQL_USER, getenv("MYSQL_USER"));
+        define($MYSQL_PASSWORD, getenv("MYSQL_PASSWORD"));
         define($DB_HOST, 'db');
         define($DB_NAME, 'db_test');
         ?>
