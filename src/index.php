47565e83eb8694e6633d1c51669ba89146bce603
Administrator <studentq.work@example.com>
2024-09-16 00:40:13 +0400

    fix variables

--- src/index.php
+++ src/index.php
@@ -23,15 +23,15 @@
         </header>
 
         <?php
-        define($MYSQL_USER, getenv("MYSQL_USER"));
-        define($MYSQL_PASSWORD, getenv("MYSQL_PASSWORD"));
-        define($DB_HOST, 'db');
-        define($DB_NAME, 'db_test');
+        $db_host = getenv("MYSQL_HOST");
+        $db_user = getenv("MYSQL_USER");
+        $db_password = getenv("MYSQL_PASSWORD");
+        $db_name = getenv("MYSQL_DB");
         ?>
 
         <?php
         
-        $conn = new mysqli($DB_HOST, $MYSQL_USER, $MYSQL_PASSWORD, $DB_NAME);
+        $conn = new mysqli($db_host, $db_user, $db_password, $db_name);
         if ($conn->connect_error) {
             die("Ошибка: " . $conn->connect_error);
         }
