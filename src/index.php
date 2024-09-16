1fe3a3b20fe44c5bc716c421efa68e83418dbf90
Administrator <studentq.work@example.com>
2024-09-16 10:18:09 +0400

    fix

--- src/index.php
+++ src/index.php
@@ -76,11 +76,11 @@ require_once __DIR__ . '/model/connect.php';
         <form action="assets/insert_sql.php" method="post" class="form-example">
             <div>
                 <label for="name">Имя: </label>
-                <input type="text" name="name" id="name" required maxlength="14"/>
+                <input type="text" name="name" id="name" required maxlength="10"/>
             </div>
             <div>
                 <label for="beer">Пиво: </label>
-                <input type="text" name="beer" id="beer" required maxlength="21"/>
+                <input type="text" name="beer" id="beer" required maxlength="10"/>
             </div>
             <div>
                 <input type="submit" value="Отправить"/>
