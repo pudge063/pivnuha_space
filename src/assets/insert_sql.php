cd01e2bb760730034e1729b5b14a7a02ebe41845
Administrator <studentq.work@example.com>
2024-09-16 23:17:36 +0400

    fix

--- src/assets/insert_sql.php
+++ src/assets/insert_sql.php
@@ -3,14 +3,13 @@
 require_once __DIR__ . '/../model/connect.php';
 // require_once __DIR__ . '/../get_ip.php';
 
-if (isset($_POST['name']) && isset($_POST['beer']) &&isset($_POST['g-recaptcha-response'])) {
+if (isset($_POST['name']) && isset($_POST['beer']) && isset($_POST['g-recaptcha-response'])) {
     // $conn = mysqli_connect("db", "admin", "123", "db_test");
     mysqli_set_charset($conn, "utf8");
 
     $name = mysqli_real_escape_string($conn, $_POST['name']);
     $beer = mysqli_real_escape_string($conn, $_POST['beer']);
-    // $ip = mysqli_real_escape_string($conn, getIp());
-    $ip = 1;
+    $ip = mysqli_real_escape_string($conn, getIp());
     $user_agent = mysqli_real_escape_string($conn, $_SERVER['HTTP_USER_AGENT']);
 
     if (mysqli_connect_errno()) {
@@ -20,14 +19,9 @@ if (isset($_POST['name']) && isset($_POST['beer']) &&isset($_POST['g-recaptcha-r
     $query1 = "INSERT INTO test1 (name, beer, ip_address, user_agent) VALUES ('$name', '$beer', '$ip', '$user_agent');";
     $result1 = mysqli_query($conn, $query1);
 
-    // if ($result1 = 'true') {
-    //     echo "Информация занесена в базу данных";
-    // } else {
-    //     echo "Информация не занесена в базу данных";
-    // }
     echo $c;
     mysqli_close($conn);
 }
 
-// header("Location: ../");
+header("Location: ../");
 exit();
