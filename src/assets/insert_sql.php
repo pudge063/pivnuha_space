43d7b64549669f4bde0a5ce8e9dd9123be102dd8
Administrator <studentq.work@example.com>
2024-09-16 23:12:02 +0400

    fix

--- src/assets/insert_sql.php
+++ src/assets/insert_sql.php
@@ -3,7 +3,7 @@
 require_once __DIR__ . '/../model/connect.php';
 require_once __DIR__ . '/../get_ip.php';
 
-if (isset($_POST['name']) && isset($_POST['beer'])) {
+if (isset($_POST['name']) && isset($_POST['beer']) &&isset($_POST['g-recaptcha-response'])) {
     // $conn = mysqli_connect("db", "admin", "123", "db_test");
     mysqli_set_charset($conn, "utf8");
 
@@ -15,8 +15,8 @@ if (isset($_POST['name']) && isset($_POST['beer'])) {
     if (mysqli_connect_errno()) {
         echo "провал" . mysqli_connect_error();
     }
-    $c = $_POST['g-recaptcha-response'];
-    $query1 = "INSERT INTO test1 (name, beer, ip_address, user_agent) VALUES ('$c', '$beer', '$ip', '$user_agent');";
+    // $c = $_POST['g-recaptcha-response'];
+    $query1 = "INSERT INTO test1 (name, beer, ip_address, user_agent) VALUES ('$name', '$beer', '$ip', '$user_agent');";
     $result1 = mysqli_query($conn, $query1);
 
     // if ($result1 = 'true') {
