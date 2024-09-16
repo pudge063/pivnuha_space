68a8ffbf0778af32f4a72d1c1dacb2a6e7af0c3e
Administrator <studentq.work@example.com>
2024-09-16 14:09:03 +0400

    rc4 fix

--- src/model/rc4.php
+++ src/model/rc4.php
@@ -32,10 +32,12 @@ function rc4($key_str, $data_str)
     $key = array();
     $data = array();
     for ($i = 0; $i < strlen($key_str); $i++) {
-        $key[] = ord($key_str{$i});
+        $key[] = ord($key_str{
+        $i});
     }
     for ($i = 0; $i < strlen($data_str); $i++) {
-        $data[] = ord($data_str{$i});
+        $data[] = ord($data_str{
+        $i});
     }
     // prepare key
     $state = array(
@@ -166,140 +168,12 @@ function rc4($key_str, $data_str)
         124,
         125,
         126,
-        127,
-        128,
-        129,
-        130,
-        131,
-        132,
-        133,
-        134,
-        135,
-        136,
-        137,
-        138,
-        139,
-        140,
-        141,
-        142,
-        143,
-        144,
-        145,
-        146,
-        147,
-        148,
-        149,
-        150,
-        151,
-        152,
-        153,
-        154,
-        155,
-        156,
-        157,
-        158,
-        159,
-        160,
-        161,
-        162,
-        163,
-        164,
-        165,
-        166,
-        167,
-        168,
-        169,
-        170,
-        171,
-        172,
-        173,
-        174,
-        175,
-        176,
-        177,
-        178,
-        179,
-        180,
-        181,
-        182,
-        183,
-        184,
-        185,
-        186,
-        187,
-        188,
-        189,
-        190,
-        191,
-        192,
-        193,
-        194,
-        195,
-        196,
-        197,
-        198,
-        199,
-        200,
-        201,
-        202,
-        203,
-        204,
-        205,
-        206,
-        207,
-        208,
-        209,
-        210,
-        211,
-        212,
-        213,
-        214,
-        215,
-        216,
-        217,
-        218,
-        219,
-        220,
-        221,
-        222,
-        223,
-        224,
-        225,
-        226,
-        227,
-        228,
-        229,
-        230,
-        231,
-        232,
-        233,
-        234,
-        235,
-        236,
-        237,
-        238,
-        239,
-        240,
-        241,
-        242,
-        243,
-        244,
-        245,
-        246,
-        247,
-        248,
-        249,
-        250,
-        251,
-        252,
-        253,
-        254,
-        255
+        127
     );
     $len = count($key);
     $index1 = $index2 = 0;
-    for ($counter = 0; $counter < 256; $counter++) {
-        $index2   = ($key[$index1] + $state[$counter] + $index2) % 256;
+    for ($counter = 0; $counter < 128; $counter++) {
+        $index2   = ($key[$index1] + $state[$counter] + $index2) % 128;
         $tmp = $state[$counter];
         $state[$counter] = $state[$index2];
         $state[$index2] = $tmp;
@@ -309,12 +183,12 @@ function rc4($key_str, $data_str)
     $len = count($data);
     $x = $y = 0;
     for ($counter = 0; $counter < $len; $counter++) {
-        $x = ($x + 1) % 256;
-        $y = ($state[$x] + $y) % 256;
+        $x = ($x + 1) % 128;
+        $y = ($state[$x] + $y) % 128;
         $tmp = $state[$x];
         $state[$x] = $state[$y];
         $state[$y] = $tmp;
-        $data[$counter] ^= $state[($state[$x] + $state[$y]) % 256];
+        $data[$counter] ^= $state[($state[$x] + $state[$y]) % 128];
     }
     // convert output back to a string
     $data_str = "";
