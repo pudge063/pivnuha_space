aaaa078f9bcde069036fa4f8f2147826d1be3e35
Administrator <studentq.work@example.com>
2024-09-25 14:33:16 +0400

    fix UserController

--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -11,46 +11,19 @@ class UserController
         $this->userModel = new User($dbConnection);
     }
 
+
+
     public function getUser($user_id)
     {
         $result = $this->userModel->get_user($user_id);
         return $result;
     }
 
-    public function loginUser($username)
+
+
+    public function loginUser($username, $password)
     {
-        $user = $this->userModel->get_user_by_username($username);
-        return $user;
-    }
 
-    public function validate($value, $field, $user_id)
-    {
-        $result = $this->userModel->isExists($value, $field, $user_id);
-        return $result;
-    }
-
-    public function registerUser($username, $name, $phone, $email, $password)
-    {
-        $this->userModel->create_user($username, $name, $phone, $email, $password);
-    }
-
-    public function updateUser($user_id, $updated_fields)
-    {
-        $this->userModel->update_user($user_id, $updated_fields);
-    }
-}
-
-$dbConnection = $conn;
-$userController = new UserController($dbConnection);
-
-if (isset($_GET['action'])) {
-    $action = $_GET['action'];
-
-    if (session_status() === PHP_SESSION_NONE) {
-        session_start();
-    }
-
-    if ($action === 'login') {
         $is_valid = validate_capthca();
 
         if ($is_valid == false || !isset($is_valid)) {
@@ -60,48 +33,28 @@ if (isset($_GET['action'])) {
             exit();
         }
 
-        if (!empty($_POST)) {
-            $username = $_POST["username"];
-            $password = $_POST["password"];
-        }
-        $user = $userController->loginUser($username, $password);
 
-        if ($user && $password == $user["password"]) {
-            $_SESSION['user_id'] = $user['id'];
-            $_SESSION['username'] = $user['username'];
-            $_SESSION['name'] = $user['name'];
 
-            header("Location: ../");
-            exit();
-        } else {
-            $errors[] = "Неверное имя пользователя или пароль.";
-            $_SESSION["errors"] = $errors;
-            header("Location: ../views/auth/login.php");
-        }
-    } elseif ($action === 'logout') {
-        $_SESSION = [];
-        session_destroy();
+        $user = $this->userModel->get_user_by_username($username, $password);
 
-        header('Location: ../../');
-        exit();
-    } elseif ($action === 'register') {
-        $is_valid = validate_capthca();
-        if ($is_valid == false) {
-            $errors[] = 'Не пройдена капча.';
-            $_SESSION['errors'] = $errors;
-            header('Location: ../views/auth/register.php');
-        }
+        return $user;
+    }
+
+
+
+    public function validate($value, $field, $user_id)
+    {
+        $result = $this->userModel->isExists($value, $field, $user_id);
+        return $result;
+    }
+
+
+
+
+    public function registerUser($username, $name, $phone, $email, $password, $passwordConfirm)
+    {
 
         $user_id = NULL;
-
-        if (!empty($_POST)) {
-            $username = $_POST['username'];
-            $name = $_POST['name'];
-            $phone = $_POST['phone'];
-            $email = $_POST['email'];
-            $password = $_POST['password'];
-            $passwordConfirm = $_POST['passwordConfirm'];
-        }
 
         if ($password != $passwordConfirm) {
             $errors[] = 'Не совпадают пароли.';
@@ -124,7 +77,7 @@ if (isset($_GET['action'])) {
             exit();
         }
 
-        $existUsername = $userController->validate($username, 'username', $user_id);
+        $existUsername = $this->validate($username, 'username', $user_id);
         if ($existUsername === false) {
             $errors[] = 'Имя пользователя занято.';
             header('Location: ../views/auth/register.php');
@@ -132,7 +85,8 @@ if (isset($_GET['action'])) {
             exit();
         }
 
-        $existPhone = $userController->validate($phone, 'phone', $user_id);
+
+        $existPhone = $this->validate($phone, 'phone', $user_id);
         if ($existPhone === false) {
             $errors[] = 'Телефон уже привязан к другому аккаунту.';
             header('Location: ../views/auth/register.php');
@@ -140,7 +94,7 @@ if (isset($_GET['action'])) {
             exit();
         }
 
-        $exitstMail = $userController->validate($email, 'email', $user_id);
+        $existMail = $this->validate($email, 'email', $user_id);
         if (isset($existMail) && $existMail === false) {
             $errors[] = 'Почта уже привязана к другому аккаунту.';
             $_SESSION['errors'] = $errors;
@@ -148,10 +102,145 @@ if (isset($_GET['action'])) {
             exit();
         }
 
-        $userController->registerUser($username, $name, $phone, $email, $password);
+        $this->userModel->create_user($username, $name, $phone, $email, $password);
+    }
+
+
+
+
+    public function updateUser($user_id, $updated_fields)
+    {
+        $this->userModel->update_user($user_id, $updated_fields);
+    }
+}
+
+$dbConnection = $conn;
+$userController = new UserController($dbConnection);
+
+if (isset($_GET['action'])) {
+    $action = $_GET['action'];
+
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
+
+    // login
+
+    if ($action === 'login') {
+
+        if (!empty($_POST)) {
+            $username = $_POST["username"];
+            $password = $_POST["password"];
+        }
+
+        $user = $userController->loginUser($username, $password);
+
+        if ($user && $password == $user["password"]) {
+            $_SESSION['user_id'] = $user['id'];
+            $_SESSION['username'] = $user['username'];
+            $_SESSION['name'] = $user['name'];
+
+            header("Location: ../");
+            exit();
+        } else {
+            $errors[] = "Неверное имя пользователя или пароль.";
+            $_SESSION["errors"] = $errors;
+            header("Location: ../views/auth/login.php");
+        }
+    }
+
+    // logout
+
+    elseif ($action === 'logout') {
+        $_SESSION = [];
+        session_destroy();
+
+        header('Location: ../../');
+        exit();
+    }
+
+    // register
+
+    elseif ($action === 'register') {
+
+        $is_valid = validate_capthca();
+
+        if ($is_valid == false) {
+            $errors[] = 'Не пройдена капча.';
+            $_SESSION['errors'] = $errors;
+            header('Location: ../views/auth/register.php');
+        }
+
+        $user_id = NULL;
+
+        if (!empty($_POST)) {
+            $username = $_POST['username'];
+            $name = $_POST['name'];
+            $phone = $_POST['phone'];
+            $email = $_POST['email'];
+            $password = $_POST['password'];
+            $passwordConfirm = $_POST['passwordConfirm'];
+        }
+
+
+        $userController->registerUser($username, $name, $phone, $email, $password, $passwordConfirm);
+
+        ///
+
+        // if ($password != $passwordConfirm) {
+        //     $errors[] = 'Не совпадают пароли.';
+        //     $_SESSION['errors'] = $errors;
+        //     header('Location: ../views/auth/register.php');
+        //     exit();
+        // }
+
+        // if (strlen($username) < 4 || strlen($username) > 15 || !preg_match('/^[a-zA-Z]+$/', $username)) {
+        //     $errors[] = "Логин только от 4 до 15 символов. Можно использовать только латиницу.";
+        //     $_SESSION['errors'] = $errors;
+        //     header('Location: ../views/auth/register.php');
+        //     exit();
+        // }
+
+        // if (strlen($phone) < 8 || strlen($phone) > 12 || !preg_match('/^[0-9]+$/', $phone)) {
+        //     $errors[] = "Телефон только от 8 до 12 символов. Можно использовать только цифры.";
+        //     $_SESSION['errors'] = $errors;
+        //     header('Location: ../views/auth/register.php');
+        //     exit();
+        // }
+
+        // $existUsername = $userController->validate($username, 'username', $user_id);
+        // if ($existUsername === false) {
+        //     $errors[] = 'Имя пользователя занято.';
+        //     header('Location: ../views/auth/register.php');
+        //     $_SESSION['errors'] = $errors;
+        //     exit();
+        // }
+
+        // $existPhone = $userController->validate($phone, 'phone', $user_id);
+        // if ($existPhone === false) {
+        //     $errors[] = 'Телефон уже привязан к другому аккаунту.';
+        //     header('Location: ../views/auth/register.php');
+        //     $_SESSION['errors'] = $errors;
+        //     exit();
+        // }
+
+        // $exitstMail = $userController->validate($email, 'email', $user_id);
+        // if (isset($existMail) && $existMail === false) {
+        //     $errors[] = 'Почта уже привязана к другому аккаунту.';
+        //     $_SESSION['errors'] = $errors;
+        //     header('Location: ../views/auth/register.php');
+        //     exit();
+        // }
+
+        // $userController->registerUser($username, $name, $phone, $email, $password, $passwordConfirm);
 
         header('Location: ../views/auth/login.php');
-    } elseif ($action === 'edit') {
+    }
+
+
+    /// edit
+
+    elseif ($action === 'edit') {
 
         if (!isset($_SESSION['user_id'])) {
             header("Location: ../../views/auth/login.php");
