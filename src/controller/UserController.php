20d99a74f84d62f2dc4e546c35315f7ddd904f2e
Administrator <studentq.work@example.com>
2024-09-25 13:37:36 +0400

    fix register

--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -1,8 +1,6 @@
 <?php
 require_once __DIR__ . '/../model/User.php';
 require_once __DIR__ . '/../captcha.php';
-// require_once __DIR__ . '/../model/Database.php';
-require_once __DIR__ . '/../connect.php';
 
 class UserController
 {
