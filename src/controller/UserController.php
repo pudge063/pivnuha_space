c68bfa9b2b1896ad19eae5cf42489894d6ba557c
Administrator <studentq.work@example.com>
2024-09-25 14:43:21 +0400

    fix UserController

--- src/controller/UserController.php
+++ src/controller/UserController.php
@@ -108,8 +108,47 @@ class UserController
 
 
 
-    public function updateUser($user_id, $updated_fields)
+    public function updateUser($user_id, $fields)
     {
+        $new_name = $fields['new_name'];
+        $new_email = $fields['email'];
+        $new_phone = $fields['phone'];
+        $new_password = $fields['password'];
+
+        if (!empty($new_name)) {
+            $updated_fields['name'] = $new_name;
+        }
+
+        $res = $this->validate($new_email, 'email', $user_id);
+
+        if ($res == false) {
+            $errors[] = "Email уже используется другим пользователем.";
+            $_SESSION['errors'] = $errors;
+            header('Location: ../../views/users/profile.php');
+            exit();
+        } else {
+            if (!empty($new_email)) {
+                $updated_fields['email'] = $new_email;
+            }
+        }
+
+        $res = $this->validate($new_phone, 'phone', $user_id);
+
+        if ($res === false) {
+            $errors[] = "Телефон уже используется другим пользователем.";
+            $_SESSION['errors'] = $errors;
+            header('Location: ../../views/users/profile.php');
+            exit();
+        } else {
+            if (!empty($new_phone)) {
+                $updated_fields['phone'] = $new_phone;
+            }
+        }
+
+        if (!empty($new_password)) {
+            $updated_fields['password'] = $new_password;
+        }
+
         $this->userModel->update_user($user_id, $updated_fields);
     }
 }
@@ -247,11 +286,10 @@ if (isset($_GET['action'])) {
             exit();
         }
 
-        $new_name = isset($_POST['name']) ? trim($_POST['name']) : null;
-        $new_email = isset($_POST['email']) ? trim($_POST['email']) : null;
-        $new_password = isset($_POST['password']) ? trim($_POST['password']) : null;
-        $new_phone = isset($_POST['phone']) ? trim($_POST['phone']) : null;
-        $new_password = isset($_POST['password']) ? $_POST['password'] : null;
+        $fields['new_name'] = $new_name = isset($_POST['name']) ? trim($_POST['name']) : null;
+        $fields['email'] = $new_email = isset($_POST['email']) ? trim($_POST['email']) : null;
+        $fields['password'] = $new_password = isset($_POST['password']) ? trim($_POST['password']) : null;
+        $fields['phone'] = $new_phone = isset($_POST['phone']) ? trim($_POST['phone']) : null;
 
 
         $user_id = $_SESSION['user_id'];
@@ -260,43 +298,47 @@ if (isset($_GET['action'])) {
 
         $current_user = $userController->getUser($user_id);
 
-        if (!empty($new_name)) {
-            $updated_fields['name'] = $new_name;
-        }
+        $userController->updateUser($user_id, $fields);
 
-        $res = $userController->validate($new_email, 'email', $user_id);
+        ///////
 
-        if ($res == false) {
-            $errors[] = "Email уже используется другим пользователем.";
-            $_SESSION['errors'] = $errors;
-            header('Location: ../../views/users/profile.php');
-            exit();
-        } else {
-            if (!empty($new_email)) {
-                $updated_fields['email'] = $new_email;
-            }
-        }
+        // if (!empty($new_name)) {
+        //     $updated_fields['name'] = $new_name;
+        // }
 
-        $res = $userController->validate($new_phone, 'phone', $user_id);
+        // $res = $userController->validate($new_email, 'email', $user_id);
 
-        if ($res === false) {
-            $errors[] = "Телефон уже используется другим пользователем.";
-            $_SESSION['errors'] = $errors;
-            header('Location: ../../views/users/profile.php');
-            exit();
-        } else {
-            if (!empty($new_phone)) {
-                $updated_fields['phone'] = $new_phone;
-            }
-        }
+        // if ($res == false) {
+        //     $errors[] = "Email уже используется другим пользователем.";
+        //     $_SESSION['errors'] = $errors;
+        //     header('Location: ../../views/users/profile.php');
+        //     exit();
+        // } else {
+        //     if (!empty($new_email)) {
+        //         $updated_fields['email'] = $new_email;
+        //     }
+        // }
 
-        if (!empty($new_password)) {
-            $updated_fields['password'] = $new_password;
-        }
+        // $res = $userController->validate($new_phone, 'phone', $user_id);
 
-        if (empty($errors) && !empty($updated_fields)) {
-            $userController->updateUser($user_id, $updated_fields);
-        }
+        // if ($res === false) {
+        //     $errors[] = "Телефон уже используется другим пользователем.";
+        //     $_SESSION['errors'] = $errors;
+        //     header('Location: ../../views/users/profile.php');
+        //     exit();
+        // } else {
+        //     if (!empty($new_phone)) {
+        //         $updated_fields['phone'] = $new_phone;
+        //     }
+        // }
+
+        // if (!empty($new_password)) {
+        //     $updated_fields['password'] = $new_password;
+        // }
+
+        // if (empty($errors) && !empty($updated_fields)) {
+        //     $userController->updateUser($user_id, $updated_fields);
+        // }
 
         if (!empty($errors)) {
             $_SESSION['errors'] = $errors;
